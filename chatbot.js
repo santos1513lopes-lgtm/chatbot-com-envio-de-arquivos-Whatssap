db20b2cfc67db858fc7f7000fa07707f4898cbff
santos1513lopes-lgtm <santos1513.lopes@gmail.com>
2026-04-22 22:23:58 -0300

    .pausado\

--- chatbot.js
+++ chatbot.js
@@ -107,7 +107,7 @@ client.on("message", async (msg) => {
           `📇 Captura automática de contatos\n` +
           `💻 Aprenda como deixar o robô funcionando 24 hrs, com o PC desligado\n` +
           `✅ E 3 Bônus exclusivos\n\n` +
-          `🔥 Adquira a versão PRO agora: https://pay.kiwify.com.br/FkTOhRZ?src=pro`
+          `🔥 Adquira a versão 
       );
     }
 
